b1c8a6a6124f5b2cd7e43529f3c6906de326e625
achu-bsl <asharudeenalikt@gmail.com>
2023-10-09 12:39:53 +0530

    Updated auth pages

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,35 +1,44 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from "../../style";
-import { PasswordInput } from "../../components/auth/PasswordInput";
-import { Button } from "../../components/auth/AuthButton";
+import { PasswordInput } from "../../components/auth/input/PasswordInput";
 import GoogleIcon from "@mui/icons-material/Google";
-import { AuthInput } from "../../components/auth/AuthInput";
-import { Link } from "react-router-dom";
+import { AuthInput } from "../../components/auth/input/AuthInput";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthErrorsInterface, LoginFormInterface } from "../../interfaces/Auth";
 import { loginSchema } from "../../utils/zodValidator";
 import { toast } from "react-toastify";
 import { useZodForm } from "../../hooks/useZodForm";
 import { useAuthErrorLog } from "../../hooks/useAuthErrorLog";
-import { axiosInstance } from "../../common/api";
 import { isAxiosError } from "axios";
-import { useAuthContext } from "../../context/useAuthContext";
-import { OTPInputContainer } from "../../components/auth/OTPInputContainer";
+import { postRequest } from "../../helper/api.helper";
+import { API_POST_LOGIN, API_POST_VALIDATE_2AF } from "../../constants/api.url";
+import { OTPCard } from "../../components/auth/OTP-card";
+import { Button } from "../../components/custom/Button";
+import { useAppDispatch } from "@hooks/storeHooks";
+import { toggleIsLoggedIn } from "@/store/slice/useSlice";
 
 export const Login: FC = () => {
-  const { isOTPFormViewble, setIsOTPFormViewble } = useAuthContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isOTPCardOpen, setIsOTPCardOpen] = useState(false);
   const { handleSubmit, register, errors } =
     useZodForm<LoginFormInterface>(loginSchema);
   useAuthErrorLog(errors as AuthErrorsInterface);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
+  console.log(isLoading);
   const FormSubmitHandler = async (data: LoginFormInterface) => {
     try {
-      const res = await axiosInstance.post("/auth/login", data);
-      console.log(res.status);
+      console.log("hello");
+      setIsLoading(true);
+      const res = await postRequest(API_POST_LOGIN, data);
       if (res.status === 200) {
         toast.success("Login success");
+        dispatch(toggleIsLoggedIn(true));
+        navigate("/personal/project");
       } else {
         toast.success("OTP sent successfully");
-        setIsOTPFormViewble(true);
+        setIsOTPCardOpen(true);
       }
     } catch (err) {
       let message = "OPPS Something wrong";
@@ -40,12 +49,16 @@ export const Login: FC = () => {
         }
       }
       toast.error(message);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <>
-      <div className={`bg-primary w-full ${styles.flexCenter} py-16 md:py-40`}>
+      <div
+        className={`bg-primary w-full min-h-screen ${styles.flexCenter} py-16 md:py-40`}
+      >
         <div
           className={` min-h-[600px] ${styles.boxWidth} ${styles.flexCenter}`}
         >
@@ -56,7 +69,11 @@ export const Login: FC = () => {
             <div
               className={`flex-1 w-full flex justify-center items-center flex-col`}
             >
-              <div className="w-full px-10 max-w-[400px]">
+              <div
+                className={`w-full px-10 max-w-[400px] ${
+                  isOTPCardOpen ? "scale-0" : "scale-100"
+                } transition-transform duration-300`}
+              >
                 <div className="md:mb-10 mb-5 w-full">
                   <h1 className="md:text-[34px] text-[24px] font-semibold font-poppins">
                     Login
@@ -66,7 +83,10 @@ export const Login: FC = () => {
                   </p>
                 </div>
                 <form onSubmit={handleSubmit(FormSubmitHandler)}>
-                  <div className="flex-1 flex md:gap-4 gap-2 flex-col font-">
+                  <div
+                    className={`flex-1 flex md:gap-4 gap-2 flex-col
+                  `}
+                  >
                     <AuthInput
                       {...register("user_email")}
                       label="Email*"
@@ -79,50 +99,39 @@ export const Login: FC = () => {
                     />
                   </div>
                   <div className="flex flex-col gap-2 my-6 md:my-12">
-                    <Button isPrimary={true} text="Login" />
-                    <Link to="/auth/register">
-                      <Button isPrimary={false} text="Sign up" />
+                    <Button
+                      type="submit"
+                      isLoading={isLoading}
+                      className="bg-light_hash rounded-3xl"
+                    >
+                      Login
+                    </Button>
+                    <Link className="w-full" to="/auth/register">
+                      <Button
+                        size="custom"
+                        color="transperant"
+                        className="w-full rounded-3xl"
+                      >
+                        Sign up
+                      </Button>
                     </Link>
                   </div>
                 </form>
                 <div className="flex justify-center md:my-10 my-4">
                   <p>or</p>
                 </div>
-                <div>
-                  <Button
-                    icon={<GoogleIcon fontSize="small" className="mr-2" />}
-                    isPrimary={true}
-                    text="Sign in with google"
-                  />
-                </div>
+                <div>{/**google */}</div>
               </div>
             </div>
             <div className={`flex-1 ${styles.flexCenter}`}>
               <div
                 className={`bg-hash_one h-[100%] w-[100%] rounded-l-xl flex-1 ${styles.flexCenter}`}
               >
-                <div className="bg-light_hash h-[400px] top-[50%] sm:top-0 translate-y-[-50%] sm:translate-y-0 sm:h-[80%] sm:w-[80%] rounded-lg absolute sm:relative">
-                  <div
-                    className={`h-full w-full ${
-                      styles.flexCenter
-                    } flex-col justify-evenly px-6 ${
-                      !isOTPFormViewble ? "hidden" : ""
-                    }`}
-                  >
-                    <div className="flex flex-col items-center">
-                      <h3 className="text-[20px] font-poppins font-semibold">
-                        2-Factor Auth
-                      </h3>
-                      <p className="text-[15px]  font-[300]">
-                        Check your email and enter your one-time-password
-                      </p>
-                    </div>
-                    <OTPInputContainer fetchUrl="/auth/validate/2AF-otp" />
-                    <div
-                      className={`${styles.flexCenter} flex-col font-poppins text-[15px] underline `}
-                    ></div>
-                  </div>
-                </div>
+                <OTPCard
+                  closeHandler={setIsOTPCardOpen}
+                  isOpen={isOTPCardOpen}
+                  validateURL={API_POST_VALIDATE_2AF}
+                />
               </div>
             </div>
           </div>
--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,26 +1,18 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from "../../style";
-import { Button } from "../../components/auth/AuthButton";
-import GoogleIcon from "@mui/icons-material/Google";
-import { OTPInputContainer } from "../../components/auth/OTPInputContainer";
 import { resendRegisterOTPHandler } from "../../helper/auth.helper";
-import { useAuthContext } from "../../context/useAuthContext";
-import { RegisterForm } from "../../components/auth/RegisterForm";
-import { useAppDispatch } from "../../hooks/storeHooks";
-import { offLoading, onLoading } from "../../store/slice/loadingSlice";
+import { RegisterForm } from "../../components/auth/form/RegisterForm";
+import { OTPCard } from "../../components/auth/OTP-card";
+import { API_POST_VALIDATE_REGISTER_OTP } from "../../constants/api.url";
 
 export const Register: FC = () => {
-  const { isOTPFormViewble, setIsOTPFormViewble } = useAuthContext();
-  const dispatch = useAppDispatch();
+  const [isRegistered, setIsRegisterd] = useState(false);
 
-  const resendOTPHandler = () => {
-    dispatch(onLoading());
-    resendRegisterOTPHandler();
-    dispatch(offLoading());
-  }
   return (
     <>
-      <div className={`bg-primary w-full ${styles.flexCenter} py-16 md:py-40 `}>
+      <div
+        className={`bg-primary min-h-screen w-full ${styles.flexCenter} py-16 `}
+      >
         <div
           className={` min-h-[600px] ${styles.boxWidth} ${styles.flexCenter}`}
         >
@@ -30,7 +22,11 @@ export const Register: FC = () => {
             <div
               className={`flex-1 w-full flex justify-center items-center flex-col`}
             >
-              <div className="w-full px-10 max-w-[400px]">
+              <div
+                className={`w-full px-10 max-w-[400px] ${
+                  isRegistered ? "scale-0" : "scale-100"
+                } transition-transform duration-300 ease-in-out`}
+              >
                 <div className="md:mb-10 mb-5 w-full">
                   <h1 className="md:text-[34px] text-[24px] font-semibold font-poppins">
                     Sign up
@@ -39,60 +35,24 @@ export const Register: FC = () => {
                     Hi, Join with us.
                   </p>
                 </div>
-                <RegisterForm />
+                <RegisterForm setIsRegistered={setIsRegisterd} />
 
                 <div className="flex justify-center md:my-10 my-4">
                   <p>or</p>
                 </div>
-                <div>
-                  <Button
-                    isDisabled={isOTPFormViewble}
-                    icon={<GoogleIcon fontSize="small" className="mr-2" />}
-                    isPrimary={true}
-                    text="Sign up with google"
-                  />
-                </div>
+                <div>{/**google */}</div>
               </div>
             </div>
             <div className={`flex-1 ${styles.flexCenter}`}>
               <div
                 className={`bg-hash_one h-[100%] w-[100%] rounded-l-xl flex-1 ${styles.flexCenter}`}
               >
-                <div className="bg-light_hash h-[400px] top-[50%] sm:top-0 translate-y-[-50%] sm:translate-y-0 sm:h-[80%] sm:w-[80%] rounded-lg absolute sm:relative">
-                  <div
-                    className={`h-full w-full ${
-                      styles.flexCenter
-                    } flex-col justify-evenly px-6 ${
-                      !isOTPFormViewble ? "hidden" : ""
-                    }`}
-                  >
-                    <div className="flex flex-col items-center">
-                      <h3 className="text-[20px] font-poppins font-semibold">
-                        Varify Email
-                      </h3>
-                      <p className="text-[15px]  font-[300]">
-                        Check your email and enter your one-time-password
-                      </p>
-                    </div>
-                    <OTPInputContainer fetchUrl="/auth/validate/register-otp"/>
-                    <div
-                      className={`${styles.flexCenter} flex-col font-poppins text-[15px] underline `}
-                    >
-                      <p
-                        className="cursor-pointer"
-                        onClick={resendOTPHandler}
-                      >
-                        resend
-                      </p>
-                      <p
-                        className="cursor-pointer"
-                        onClick={() => setIsOTPFormViewble(false)}
-                      >
-                        Change email
-                      </p>
-                    </div>
-                  </div>
-                </div>
+                <OTPCard
+                  isOpen={isRegistered}
+                  closeHandler={setIsRegisterd}
+                  resendOTPFn={resendRegisterOTPHandler}
+                  validateURL={API_POST_VALIDATE_REGISTER_OTP}
+                />
               </div>
             </div>
           </div>
