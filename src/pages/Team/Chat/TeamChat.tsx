3ac89a281901d643ec61322371194fb39e7a52e9
achu-bsl <asharudeenalikt@gmail.com>
2023-11-10 22:31:22 +0530

    Implemented user typing indicator functionality

--- src/pages/Team/Chat/TeamChat.tsx
+++ src/pages/Team/Chat/TeamChat.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@components/custom/Button";
-import { FC, useEffect, useRef, useState } from "react";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
 import moment from "moment";
 import { useParams } from "react-router-dom";
 import { useUserStore } from "@/store/useUserStore";
@@ -19,28 +19,52 @@ interface MessageInterface {
 export const TeamChat: FC = () => {
   const { team_id } = useParams();
   const messageInput = useRef<HTMLInputElement>(null);
+  const [isTyping, setIsTyping] = useState(false);
+  const [isSomeoneTyping, setIsSomeoneTyping] = useState<string | null>(null);
 
   const user_id = useUserStore((state) => state.user?.user_id);
   const { socket } = useSocket();
+
   const [messages, setMessages] = useState<MessageInterface[]>([]);
 
+  const handleTeamChatTyping = useCallback(
+    ({ user_name }: { user_name: string }) => {
+      setIsSomeoneTyping(user_name);
+
+      setTimeout(() => {
+        setIsSomeoneTyping(null);
+      }, 2000);
+    },
+    []
+  );
 
   useEffect(() => {
     socket.on("team:message:receive", (message: MessageInterface) => {
-      console.log(`team:message:receive`);
+      console.log(message);
       setMessages((prev) => [...prev, message]);
     });
 
     socket.on("team:chats", (messages: MessageInterface[]) => {
-      console.log(`Team chats`);
+      console.log(`Team chats, `);
       setMessages(messages);
     });
+
+    // socket.on("team:video-call:user-joined", handleVideoCallUserJoined);
+    socket.on("team:chat:typing", handleTeamChatTyping);
   }, [socket]);
 
   useEffect(() => {
     socket.emit("team:chat:join", { team_id });
   }, []);
 
+  const messageInputOnChangeHandler = () => {
+    if (messageInput.current!.value) {
+      if (!isTyping) setIsTyping(true);
+      socket.emit("team:chat:typing", { team_id });
+    } else {
+      if (isTyping) setIsTyping(false);
+    }
+  };
 
   const sendButtonClickHandler = () => {
     const message_text = messageInput.current!.value;
@@ -59,21 +83,22 @@ export const TeamChat: FC = () => {
     messageInput.current!.value = "";
   };
 
+
   return (
-    <div className="text-white px-16 py-6 flex flex-col justify-between min-h-screen">
+    <div className="dark:text-white text-light_mode_text px-16 py-6 flex flex-col justify-between min-h-screen">
       <div className="flex-1 flex flex-col gap-3 py-10 h-full">
         <div>
           <h2 className="font-medium text-2xl">Team Chat</h2>
         </div>
         {messages.map((message) => (
           <div
-            className={`font-poppins bg-dark_hash px-4 py-2 w-[45%] rounded-xl ${
+            className={`font-poppins bg-light_mode_primary dark:bg-dark_hash px-4 py-2 w-[45%] rounded-xl ${
               message.chatter.user_id === user_id ? "self-end" : ""
             }`}
           >
             <div className="flex gap-3 items-center">
               <span className="font-medium text-medium ">
-                {message.chatter.user_id === user_id
+                {message?.chatter.user_id === user_id
                   ? "You"
                   : message.chatter.user_name}
               </span>
@@ -86,21 +111,35 @@ export const TeamChat: FC = () => {
             </div>
           </div>
         ))}
+        {isSomeoneTyping && (
+          <div className="text-white h-[10px]">
+            <div>
+              <span>{isSomeoneTyping} is typing...</span>
+            </div>
+          </div>
+        )}
       </div>
       <div className="flex gap-2 items-center">
         <Textarea
           classNames={{
+            label: ["dark:text-white", "text-light_mode_text"],
             inputWrapper: [
-              "bg-opacity-30",
-              "bg-light_hash",
-              "group-data-[focus=true]:bg-opacity-20",
-              "group-data-[hover=true]:bg-opacity-30",
-              "pe-14",
+              "dark:bg-light_hash",
+              "bg-light_mode_secondary",
+              "dark:data-[hover=true]:bg-hash_two",
+              "data-[hover=true]:bg-light_mode_tertiary",
+              "dark:group-data-[focus=true]:bg-hash_two",
+              "group-data-[focus=true]:bg-light_mode_tertiary",
             ],
-            label: ["font-[500]", "font-poppins"],
-            input: ["text-white", "text-lg", "font-poppins"],
+            innerWrapper: ["focus-within:bg-red-500"],
+            input: ["text-medium", "font-poppins"],
+          }}  
+  
+          onKeyUp={(e) => {
+            if (e.key === "Enter") sendButtonClickHandler();
           }}
           ref={messageInput}
+          onChange={messageInputOnChangeHandler}
         />
         <div className="flex flex-col gap-2">
           <Button className="w-full" onClick={sendButtonClickHandler}>
@@ -108,6 +147,7 @@ export const TeamChat: FC = () => {
           </Button>
         </div>
       </div>
+     
     </div>
   );
 };
