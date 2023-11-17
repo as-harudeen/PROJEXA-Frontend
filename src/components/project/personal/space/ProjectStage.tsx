60e2eb14993e2174b0d59eae3c3a4d27a033ed71
achu-bsl <asharudeenalikt@gmail.com>
2023-11-17 23:21:05 +0530

    Implement delete personal project stage functionality

--- src/components/project/personal/space/ProjectStage.tsx
+++ src/components/project/personal/space/ProjectStage.tsx
@@ -2,13 +2,14 @@ import { Button } from "@components/custom/Button";
 import { Input } from "@components/custom/Input";
 import { ProjectStageInterface } from "interfaces/project/personal/space/stage.interface";
 import React, { forwardRef, useRef } from "react";
-import { postRequest } from "../../../../helper/api.helper";
 import { API_POST_CREATE_STAGE_TASK } from "@/constants/api.url";
 import { useParams } from "react-router-dom";
 
 import { Draggable } from "react-beautiful-dnd";
 import { TaskDetailsInterface } from "@pages/Personal-Project/ProjectSpace";
 import { useQueryClient } from "@tanstack/react-query";
+import { useFetch } from "@hooks/useFetch";
+import { DeleteConfirmModal } from "@components/project/DeleteConfirmModal";
 
 type ProjectStageProps = {
   setTaskDetails: React.Dispatch<
@@ -25,9 +26,11 @@ export const ProjectStage = forwardRef<HTMLDivElement, ProjectStageProps>(
     const queryClient = useQueryClient();
     const taskInpRef = useRef<HTMLInputElement>(null);
     const { project_id } = useParams();
+    const { postRequest, deleteRequest } = useFetch();
 
     const addTaskHandler = async () => {
       const taskTitle = taskInpRef.current!.value;
+      taskInpRef.current!.value = "";
       const temp_task_id = Date.now().toString();
       const newTask = {
         task_id: temp_task_id,
@@ -49,7 +52,7 @@ export const ProjectStage = forwardRef<HTMLDivElement, ProjectStageProps>(
         { task_title: taskTitle }
       );
       //todo change the response type to another folder
-      const task_id = (res.data as { task_id: string }).task_id;
+      const task_id = ((await res.json()) as { task_id: string }).task_id;
 
       queryClient.setQueryData(
         ["project", "personal", "stages"],
@@ -69,7 +72,6 @@ export const ProjectStage = forwardRef<HTMLDivElement, ProjectStageProps>(
             return stage;
           })
       );
-      taskInpRef.current!.value = "";
     };
 
     const taskOnClickHandler = ({
@@ -83,13 +85,35 @@ export const ProjectStage = forwardRef<HTMLDivElement, ProjectStageProps>(
     }) => {
       setTaskDetails({ stage_id, task_id, task_title, task_desc });
     };
+
+    const deleteStageHandler = async () => {
+      const res = await deleteRequest(`project-stage/${stage_id}`);
+      if (res.ok) {
+        queryClient.setQueryData(
+          ["project", "personal", "stages"],
+          (prev: ProjectStageInterface[]) =>
+            prev.filter((stage) => stage.stage_id !== stage_id)
+        );
+      }
+    };
+
     return (
       <div
         {...props}
-        className="border-2 border-white px-6 py-4 h-full w-[360px] rounded-md bg-hash_one bg-opacity-50"
+        className="relative border h-full border-light_mode_text dark:border-white px-6 py-4 w-[300px] md:w-[360px] rounded-md bg-light_mode_primary dark:bg-hash_one bg-opacity-50 max-h-screen overflow-y-scrol shadow-lg"
       >
+        <div className="absolute right-1">
+          <DeleteConfirmModal
+            title="Are you sure?"
+            body="All tasks of this stage will also will be deleted!. 
+            are you really want delete the stage? "
+            confirmButtonHandler={deleteStageHandler}
+          />
+        </div>
         <div className="mb-4">
-          <h6 className="text-white font-semibold">{stage_title}</h6>
+          <h6 className="text-light_mode_text dark:text-white font-semibold">
+            {stage_title}
+          </h6>
         </div>
         <div>
           <div ref={ref} className="flex flex-col gap-3 min-h-[50px]">
@@ -105,7 +129,7 @@ export const ProjectStage = forwardRef<HTMLDivElement, ProjectStageProps>(
                     {...provided.dragHandleProps}
                     {...provided.draggableProps}
                     ref={provided.innerRef}
-                    className="border-2 border-white rounded-md px-4 py-2 max-h-[100px] break-all overflow-auto"
+                    className="border border-light_mode_text dark:border-white rounded-md px-2 md:px-4 py-2 max-h-[100px] break-all shadow-md"
                   >
                     {task.task_title}
                   </div>
@@ -114,17 +138,24 @@ export const ProjectStage = forwardRef<HTMLDivElement, ProjectStageProps>(
             ))}
             {placeholder}
           </div>
-          <Input
-            ref={taskInpRef}
-            label="Add task"
-            placeholder=""
-            color="border"
-            variant="underlined"
-          />
-          <Button onClick={addTaskHandler}>Add</Button>
-          <Button onClick={() => (taskInpRef.current!.value = "")}>
-            Cancel
-          </Button>
+          <div className="flex flex-col gap-2">
+            <Input
+              ref={taskInpRef}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") addTaskHandler();
+              }}
+              label="Add task"
+              placeholder=""
+              color="border"
+              variant="underlined"
+            />
+            <div className="flex gap-2">
+              <Button onClick={addTaskHandler}>Add</Button>
+              <Button onClick={() => (taskInpRef.current!.value = "")}>
+                Cancel
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     );
