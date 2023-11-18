79e444d5f025dfca934474c0bdcb7222942b8204
achu-bsl <asharudeenalikt@gmail.com>
2023-11-18 12:22:16 +0530

    Added delete functionality on team project stage

--- src/components/project/team/task-distribution/Task-distribution-stage.tsx
+++ src/components/project/team/task-distribution/Task-distribution-stage.tsx
@@ -1,26 +1,27 @@
 import { Button } from "@components/custom/Button";
 import { Input } from "@components/custom/Input";
-import {
-  TeamTaskInterface,
-  useTeamTaskDistribution,
-} from "@hooks/team/useTeamTaskDistribution";
+import { useTeamTaskDistribution } from "@/hooks/project/team-project/useTeamTaskDistribution";
 import { Select, SelectItem } from "@nextui-org/react";
 
 import { ChangeEvent, FC, useState } from "react";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 import { useParams } from "react-router-dom";
 import { TaskCard } from "./Task-Card";
+import { TaskDetails } from "@hooks/project/team-project/useTeamUsersTasks";
+import { DeleteConfirmModal } from "@components/project/DeleteConfirmModal";
+import { useFetch } from "@hooks/useFetch";
+import { toast } from "react-toastify";
 
 interface TaskDistributionStageProps {
   stage_id: string;
   stage_title: string;
-  tasks: TeamTaskInterface[];
+  tasks: TaskDetails[];
 }
 
 interface TaskStateInterface {
-  task_id: string;
   task_title: string;
   task_priority: "1" | "2" | "3" | "4" | "5";
+  task_time_cap: string;
 }
 
 export const TaskDistributionStage: FC<TaskDistributionStageProps> = ({
@@ -29,17 +30,19 @@ export const TaskDistributionStage: FC<TaskDistributionStageProps> = ({
   tasks,
 }) => {
   const [taskState, setTaskState] = useState<TaskStateInterface>({
-    task_id: "temp_task_id",
     task_title: "",
     task_priority: "5",
+    task_time_cap: "2",
   });
 
   const { team_id, project_id } = useParams();
+  const { deleteRequest } = useFetch();
 
-  const { addNewTaskDistributionStageTask } = useTeamTaskDistribution({
-    team_id: team_id!,
-    project_id: project_id!,
-  });
+  const { addNewTaskDistributionStageTask, deleteStage } =
+    useTeamTaskDistribution({
+      team_id: team_id!,
+      project_id: project_id!,
+    });
 
   const taskTitleInputChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.currentTarget.value;
@@ -65,19 +68,50 @@ export const TaskDistributionStage: FC<TaskDistributionStageProps> = ({
       stage_id,
       task_title: taskState.task_title,
       task_priority: taskState.task_priority,
+      task_time_cap: taskState.task_time_cap,
     });
-    setTaskState({
-      task_id: "temp_task_id",
-      task_title: "",
-      task_priority: "5",
-    });
+    setTaskState((prev) => ({ ...prev, task_title: "" }));
+  };
+
+  const taskTimeCapChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value;
+    if (!value || !isNaN(+value)) {
+      setTaskState((prev) => ({ ...prev, task_time_cap: value }));
+    }
+  };
+
+  // const taskTimeCapOnBlurHandler = (
+  //   e: React.FocusEvent<HTMLInputElement, Element>
+  // ) => {
+  //   const value = e.currentTarget.value;
+  //   console.log(value);
+  //   if (value === "") {
+  //     setTaskState((prev) => ({ ...prev, task_time_cap: "2" }));
+  //   }
+  // };
+
+  const deleteStageHandler = async () => {
+    const res = await deleteRequest(`team/task-distribution/stage/${stage_id}`);
+    if (res.ok) {
+      deleteStage.mutate({ stage_id });
+    } else {
+      toast.error("OPPS Something went wrong");
+    }
   };
 
   return (
     <div
       key={stage_id}
-      className="max-h-[350px] h-full overflow-y-scroll no-scrollbar w-[300px] px-6 py-4 bg-hash_one rounded-md border-1"
+      className="relative max-h-[350px] h-full overflow-y-scroll no-scrollbar w-[300px] px-6 py-4 shadow-lg bg-light_mode_primary dark:bg-hash_one rounded-md border-1 border-light_mode_text"
     >
+      <div className="absolute right-1">
+        <DeleteConfirmModal
+          title="Are you sure?"
+          body="All tasks of this stage will also will be deleted!. 
+            are you really want delete the stage? "
+          confirmButtonHandler={deleteStageHandler}
+        />
+      </div>
       <div>
         <h4 className="font-normal text-large">{stage_title}</h4>
       </div>
@@ -101,12 +135,9 @@ export const TaskDistributionStage: FC<TaskDistributionStageProps> = ({
                       {...provider.draggableProps}
                       ref={provider.innerRef}
                       key={task.task_id}
-                      className={`border-1 w-full ps-4 py-2 rounded-md break-all flex justify-between`}
+                      className={`border-1 border-light_mode_text dark:border-white bg-light_mode_hard shadow-lg w-full ps-4 py-2 rounded-md break-all flex justify-between`}
                     >
-                      <TaskCard
-                        task_title={task.task_title}
-                        task_priority={+task.task_priority}
-                      />
+                      <TaskCard stage_id={stage_id} taskDetails={task} />
                     </div>
                   )}
                 </Draggable>
@@ -116,24 +147,27 @@ export const TaskDistributionStage: FC<TaskDistributionStageProps> = ({
           )}
         </Droppable>
         {taskState.task_title && (
-          <div className="border-1 w-full px-4 py-2 rounded-md break-all">
+          <div className="border-1 border-light_mode_text dark:border-white w-full px-4 py-2 rounded-md break-all">
             <h6>{taskState.task_title}</h6>
           </div>
         )}
       </div>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 mt-10">
         <div className="flex flex-col gap-2">
           <div className="flex-1">
             <Input
               value={taskState.task_title}
               onChange={taskTitleInputChangeHandler}
+              onKeyUp={(e) => {
+                if (e.key === "Enter") addTaskButtonClickHandler();
+              }}
               size="sm"
               label="new task"
               color="border"
               classNames={{
                 inputWrapper: [
-                  "bg-opacity-30",
-                  "bg-light_hash",
+                  "dark:bg-opacity-30",
+                  "dark:bg-light_hash",
                   "group-data-[focus=true]:bg-opacity-20",
                   "group-data-[hover=true]:bg-opacity-30",
                   "pe-14",
@@ -142,7 +176,7 @@ export const TaskDistributionStage: FC<TaskDistributionStageProps> = ({
               }}
             />
           </div>
-          <div className="text-black">
+          <div className="text-black flex gap-2">
             <Select
               label="Priority"
               onChange={taskPrioritySelectorChangeHandler}
@@ -168,6 +202,20 @@ export const TaskDistributionStage: FC<TaskDistributionStageProps> = ({
                 {"5"}
               </SelectItem>
             </Select>
+
+            <Input
+              type="number"
+              size="sm"
+              label="Time Cap"
+              value={taskState.task_time_cap}
+              // onBlur={taskTimeCapOnBlurHandler}
+              onChange={taskTimeCapChangeHandler}
+              endContent={
+                <div className="flex items-end h-full">
+                  <span>h</span>
+                </div>
+              }
+            />
           </div>
         </div>
         <div>
