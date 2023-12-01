01ed4df656eb25828095528147050ac7c43e7c4c
achu-BSL <asharudeenalikt@gmail.com>
2023-12-01 15:38:01 +0530

    update useUsers hook

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -1,20 +1,17 @@
-import { useInfiniteQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { useFetch } from "./useFetch";
 import { useState } from "react";
 
-type GETgetAllUsersResponseType = {
+export type GETgetAllUsersResponseType = {
   user_name: string;
   user_profile: string;
   user_id: string;
 }[];
 
 export const useUsers = () => {
-  const queryClient = useQueryClient();
   const { getRequest} = useFetch();
 
   const [search, setSearch] = useState("");
-
-
 
   const fetchData = async ({ pageParam = 1 }) => {
     const response = await getRequest(
@@ -24,7 +21,6 @@ export const useUsers = () => {
     );
     return response.json();
   };
-
 
 
   const usersQuery = useInfiniteQuery({
@@ -40,39 +36,8 @@ export const useUsers = () => {
     },
   });
 
-
-  const fetchMoreDataMutation = useMutation({
-    mutationKey: ["users"],
-    mutationFn: async ({
-      currPage,
-      searchValue,
-    }: {
-      currPage: number;
-      searchValue: string;
-    }) => {
-      const res = await getRequest(
-        `user/get-all-users?p=${currPage}&l=${
-          import.meta.env.VITE_FETCH_USERS_LIMIT
-        }&s=${searchValue || ""}`
-      );
-      return (await res.json()) as GETgetAllUsersResponseType;
-    },
-    onSuccess(data) {
-      if ((data as GETgetAllUsersResponseType).length === 0) {
-        return;
-      }
-      queryClient.setQueryData(
-        ["users"],
-        (prev: GETgetAllUsersResponseType) => {
-          return prev ? [...prev, ...data] : [...data]
-        }
-      );
-    },
-  });
-
   return {
     usersQuery,
-    fetchMoreDataMutation,
     setSearch
   }
 };
