07d101a3dc3b8cffce25891663add0c239469d48
achu-BSL <asharudeenalikt@gmail.com>
2023-12-02 21:26:09 +0530

    optimise infinite scroll on team overview page

--- src/pages/Team/Team-Overview.tsx
+++ src/pages/Team/Team-Overview.tsx
@@ -1,37 +1,44 @@
 import { no_data } from "@/assets";
-import { IoReloadSharp } from "react-icons/io5";
-import { FC } from "react";
+import { ChangeEvent, FC, useEffect } from "react";
 import { InvitationInbox } from "@components/team/InvitationInbox";
-import InfiniteScroll from "react-infinite-scroll-component";
 import { MoonLoader } from "react-spinners";
 import { RiSearchLine } from "react-icons/ri";
 import { Input } from "@components/custom/Input";
 import { Loading } from "@components/project/Loading";
 import { TeamInterface, useTeam } from "@hooks/team/useTeams";
-import { useInfiniteScroll } from "@hooks/useInfiniteScroll";
 import { TeamCard } from "@components/project/team/TeamCard";
+import { useInView } from "react-intersection-observer";
 
 export const TeamOverview: FC = () => {
+  const { ref, inView } = useInView();
+
   const {
-    teamQuery: { data, isLoading },
-    fetchMoreDataMutation,
-    QUERY_KEY,
+    teamQuery: {
+      data,
+      isLoading,
+      fetchNextPage,
+      hasNextPage,
+      isFetchingNextPage,
+    },
+    setSearch,
   } = useTeam();
 
-  const { searchInputChangeHandler, increasePage, hasMore, searchInputRef } =
-    useInfiniteScroll<TeamInterface>(QUERY_KEY, fetchMoreDataMutation);
-
-  // const s = useUrlQuery();
-
-  // console.log(s.get('s'));
-
-  const generateRandom = () => {
-    let key = "";
-    for (let i = 0; i < 20; i++) {
-      key += Math.floor(Math.random() * 9);
+  useEffect(() => {
+    if (inView) {
+      console.log("Fetching");
+      fetchNextPage();
     }
-    return key;
+  }, [inView, hasNextPage, fetchNextPage]);
+
+  let time: NodeJS.Timeout;
+  const onChangeHandler = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (time) clearTimeout(time);
+    const searchVal = e.currentTarget.value;
+    time = setTimeout(() => {
+      setSearch(searchVal);
+    }, 1000);
   };
+
   return (
     <div className="text-light_mode_text dark:text-white px-16 py-12 flex flex-col gap-5">
       {isLoading && <Loading />}
@@ -42,8 +49,7 @@ export const TeamOverview: FC = () => {
         </div>
       </div>
       <Input
-        ref={searchInputRef}
-        onChange={searchInputChangeHandler}
+      onChange={onChangeHandler}
         label="Search"
         radius="lg"
         color="hash"
@@ -61,37 +67,35 @@ export const TeamOverview: FC = () => {
         }
       />
 
-      {data?.length === 0 && (
+      {data?.pages.length === 0 && (
         <div className="flex justify-center">
           <img className="w-[700px]" src={no_data} />
         </div>
       )}
 
-      {data?.length && (
+      {data?.pages.length && (
         <div
           id="scrollableDiv"
           className="relative px-8 no-scrollbar py-8 w-full h-[800px] bg-light_mode_secondary dark:bg-hash_one rounded-md overflow-y-scroll"
         >
-          <InfiniteScroll
-            dataLength={data.length}
-            next={increasePage}
-            hasMore={hasMore}
-            loader={
-              <div className="flex justify-center py-20 ">
-                <MoonLoader color="white" />
-              </div>
-            }
-            scrollableTarget="scrollableDiv"
-          >
-            <div className="flex flex-wrap gap-3 justify-center my-8">
-              {data.map((team) => (
-                <TeamCard key={generateRandom()} {...team} />
-              ))}
-            </div>
-          </InfiniteScroll>
-          {hasMore && (
-            <div className="fixed bottom-[10%] right-[9%] bg-light_mode_hard dark:bg-black p-2 rounded-xl">
-              <IoReloadSharp size="28" />
+          <div className="flex flex-wrap gap-3 justify-center my-8">
+            {data.pages.map((pages, i) =>
+              pages.map((team: TeamInterface, idx: number) => (
+                <TeamCard
+                  innerRef={
+                    data.pages.length - 1 === i && pages.length - 1 === idx
+                      ? ref
+                      : null
+                  }
+                  key={team.team_id}
+                  {...team}
+                />
+              ))
+            )}
+          </div>
+          {isFetchingNextPage && (
+            <div className="flex justify-center py-20 ">
+              <MoonLoader color="white" />
             </div>
           )}
         </div>
@@ -99,3 +103,6 @@ export const TeamOverview: FC = () => {
     </div>
   );
 };
+
+{
+}
